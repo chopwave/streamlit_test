e261634ec31dc1e4fa45cc1329687c0c20c9e390
chopwave <chopwave@gmail.com>
2024-03-03 13:20:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 input_num = st.number_input('Input a number', value=0)
 
